7650c52be6f4aff3988d8fb5307ac40d875bb582
sabarledave2-commits <sabarledave2@gmail.com>
2026-04-05 16:49:53 +0800

    Add files via upload

---
+++ Dump20260406/game_review_and_rating_system_db_reviews.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.45, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: game_review_and_rating_system_db
+-- ------------------------------------------------------
+-- Server version	8.0.45
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `reviews`
+--
+
+LOCK TABLES `reviews` WRITE;
+/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
+INSERT INTO `reviews` VALUES (1,1,1,10,'One of the best games ever made!','2026-03-31 16:57:36'),(2,1,2,9,'Incredible storytelling and world-building.','2026-03-31 16:57:36'),(3,2,2,10,'A masterpiece of exploration and challenge.','2026-03-31 16:57:36'),(4,3,3,7,'Great visuals, but still has some bugs.','2026-03-31 16:57:36'),(5,4,1,9,'Emotional journey and satisfying combat.','2026-03-31 16:57:36'),(6,1,4,9,'nice g','2026-03-31 17:01:42'),(9,2,4,9,'good','2026-04-01 09:49:19'),(10,4,4,10,'10 or 10 the game is good','2026-04-02 02:04:08'),(11,1,4,8,'its good not bad','2026-04-02 02:04:44'),(12,3,4,8,'not bad just med','2026-04-02 02:07:22'),(15,7,9,9,'not bad','2026-04-02 03:38:49'),(16,8,9,8,'good game but need more mode','2026-04-02 03:45:19'),(17,9,9,8,'nice game ','2026-04-02 03:47:41'),(18,10,9,7,'not bad good graphics do','2026-04-02 03:49:49'),(20,11,9,9,'best shooter games ever','2026-04-02 03:53:21'),(22,14,9,8,'its ok because its fun','2026-04-02 04:00:03'),(23,15,9,5,'its bad for the kids','2026-04-02 04:01:51'),(24,16,9,6,'nah its med but its ok i guest','2026-04-02 04:03:51'),(27,19,9,8,'this game is good for a shooter game plyer','2026-04-02 04:11:13'),(28,20,9,9,'I love it but need more muds','2026-04-02 04:14:19'),(33,14,4,10,'nicee','2026-04-04 04:11:26'),(34,2,4,9,'I like the game\n','2026-04-04 04:20:02'),(35,1,4,10,'not bad highly recommended','2026-04-04 04:22:45'),(41,23,4,9,'OG game for me bro','2026-04-04 07:26:51'),(42,23,4,10,'best game for me','2026-04-04 07:27:10'),(46,24,4,9,'I like shooting games','2026-04-04 07:31:13'),(47,25,4,8,'I like this game for the thrill','2026-04-04 07:34:40'),(48,26,4,6,'not bad nut need more game action','2026-04-04 07:37:58'),(49,27,4,5,'not for me ','2026-04-04 07:41:05'),(50,28,4,9,'I love this game so much','2026-04-04 07:43:50'),(51,29,4,8,'not bad for a shooting game\n','2026-04-04 07:46:46'),(52,30,4,7,'med for me','2026-04-04 07:48:50'),(54,31,4,9,'love it best horror game for me','2026-04-04 07:51:46'),(55,32,4,9,'this game is fun ','2026-04-04 07:55:24'),(56,33,4,8,'I like this remake','2026-04-04 07:59:46'),(57,34,4,7,'Beautiful visuals and combat Repetitive side missions','2026-04-04 08:07:46'),(59,35,4,8,'Stunning visual Repetitive races best racing games ','2026-04-04 08:10:33'),(60,36,4,7,'Unique concept but the  Dialogue is and issues','2026-04-04 08:13:19'),(61,37,4,9,'I love this horror game ','2026-04-04 08:15:58'),(62,38,4,8,'I love this survival horror game','2026-04-04 08:18:34'),(63,39,4,8,'I love this action horror game','2026-04-04 08:21:16'),(66,40,4,9,'10 over 10 I love tekken 7','2026-04-04 08:23:47'),(67,41,4,8,'best sports game ever','2026-04-04 08:26:34'),(68,42,4,8,'love it','2026-04-04 08:28:32'),(69,43,4,7,'I love walking dead hahahahah','2026-04-04 08:31:27'),(70,44,4,9,'I this give can wait to release a new version','2026-04-04 08:33:39'),(71,45,4,7,'give it a 7 need more design','2026-04-04 08:36:02'),(72,46,4,9,'love it I like western  game','2026-04-04 08:39:29'),(73,47,4,5,'its ok ','2026-04-04 08:41:56'),(74,47,4,6,'its ok','2026-04-04 08:43:09'),(75,48,4,9,'I love this horror game','2026-04-04 08:46:00'),(76,49,4,8,'LOVE THIS sequel','2026-04-04 08:48:00'),(77,50,4,9,'NOSTALGIC GAME FOR ME','2026-04-04 08:49:59'),(78,51,4,8,'SINCE 2016 ','2026-04-04 08:52:17'),(79,52,4,5,'NOT BAD I GUEST','2026-04-04 08:54:04'),(81,53,4,8,'NOSTALGIC GAME EVER','2026-04-04 08:55:45'),(82,12,4,8,'LOVE IT','2026-04-04 09:02:26'),(83,18,4,9,'BEST WESTERN GAME EVER BY ROCKSTAR','2026-04-04 09:13:50'),(84,5,4,8,'I LOVE HALO','2026-04-04 09:14:18'),(85,6,4,9,'NOSTALGIC EVER','2026-04-04 09:14:44'),(86,17,4,7,'NOT BAD BUT NEED MORE GRAPHCS','2026-04-04 09:15:34');
+/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-05 11:22:03
